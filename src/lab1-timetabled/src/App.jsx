e2ad078f1b8ca17f1220d4d82cce8fc68be2d94d
Krish A. Patel <krishpatel2067@gmail.com>
2026-05-30 09:49:04 -0400

    Lab1: Init; start making changes

---
+++ src/lab1-timetabled/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>University Class Schedule</h1>
+      <h2>Remembering all your classes and locations can be tricky. But no problem - we got you covered!</h2>
+    </div>
+  )
+}
+
+export default App
