20d3fb8d7d01eae3635a2b2d0a964e4dcecca7c2
Krish A. Patel <krishpatel2067@gmail.com>
2026-06-05 17:49:37 -0400

    Lab2: Finish Step 4

--- src/lab2-samosa-selector/src/App.jsx
+++ src/lab2-samosa-selector/src/App.jsx
@@ -2,16 +2,42 @@ import samosa from "./assets/samosa.png";
 import "./App.css";
 import { useState } from "react";
 
-const UPGRADES = [
-  { name: "Double Stuffed 👯‍♀️", desc: "2x per click", cost: "10 samosas" },
-  { name: "Party Pack 🎉", desc: "5x per click", cost: "100 samosas" },
-  { name: "Full Feast 👩🏽‍🍳", desc: "10x per click", cost: "1000 samosas" },
-];
-
 const App = () => {
   const [count, setCount] = useState(0);
   const [multiplier, setMultiplier] = useState(1);
   const updateCount = () => setCount((prev) => prev + multiplier);
+  const UPGRADES = [
+    {
+      name: "Double Stuffed 👯‍♀️",
+      desc: "2x per click",
+      cost: "10 samosas",
+      func: () => {
+        if (count >= 10) {
+          setMultiplier(multiplier * 2);
+        }
+      },
+    },
+    {
+      name: "Party Pack 🎉",
+      desc: "5x per click",
+      cost: "100 samosas",
+      func: () => {
+        if (count >= 100) {
+          setMultiplier(multiplier * 5);
+        }
+      },
+    },
+    {
+      name: "Full Feast 👩🏽‍🍳",
+      desc: "10x per click",
+      cost: "1000 samosas",
+      func: () => {
+        if (count >= 1000) {
+          setMultiplier(multiplier * 10);
+        }
+      },
+    },
+  ];
 
   return (
     <div className="App">
@@ -22,11 +48,11 @@ const App = () => {
       </div>
 
       <div className="container">
-        {UPGRADES.map((upgrade) => (
-          <div className="upgrade">
+        {UPGRADES.map((upgrade, i) => (
+          <div className="upgrade" key={i}>
             <h3>{upgrade.name}</h3>
             <p>{upgrade.desc}</p>
-            <button>{upgrade.cost}</button>
+            <button onClick={upgrade.func}>{upgrade.cost}</button>
           </div>
         ))}
       </div>
