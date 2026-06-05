1caa790086841f6bc4c6d29455339707e7369526
Krish A. Patel <krishpatel2067@gmail.com>
2026-06-05 14:56:08 -0400

    Lab2: Finish Step 2; remove unneeded default items

--- src/lab2-samosa-selector/src/App.jsx
+++ src/lab2-samosa-selector/src/App.jsx
@@ -1,13 +1,18 @@
 import samosa from "./assets/samosa.png";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  const updateCount = () => setCount((prev) => prev + multiplier);
+
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2>Count: </h2>
-        <img className="samosa" src={samosa} />
+        <h2>Count: {count}</h2>
+        <img className="samosa" src={samosa} onClick={updateCount} />
       </div>
     </div>
   );
