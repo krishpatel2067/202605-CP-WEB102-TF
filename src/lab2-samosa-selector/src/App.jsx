7ca3f7d915cdbd9575b7d902ecde8cfa84eb0cb0
Krish A. Patel <krishpatel2067@gmail.com>
2026-06-05 14:50:51 -0400

    Lab2: Finish Step 1

--- src/lab2-samosa-selector/src/App.jsx
+++ src/lab2-samosa-selector/src/App.jsx
@@ -1,7 +1,16 @@
+import samosa from "./assets/samosa.png";
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: </h2>
+        <img className="samosa" src={samosa} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
