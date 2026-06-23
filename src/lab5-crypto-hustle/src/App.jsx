b5e4fd22d28be63844fc6b1c6549b5292249818f
Krish A. Patel <krishpatel2067@gmail.com>
2026-06-23 16:16:34 -0400

    Step 1 (Query the CoinGecko API to get a list of coins): Finish

--- src/lab5-crypto-hustle/src/App.jsx
+++ src/lab5-crypto-hustle/src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
-  return <></>;
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1",
+        {
+          headers: {
+            "x-cg-demo-api-key": API_KEY,
+          },
+        },
+      );
+      const data = await response.json();
+      console.log(data);
+      setList(data);
+    };
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <ul>
+        {list?.map((coin) => (
+          <li key={coin.id}>{coin.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
