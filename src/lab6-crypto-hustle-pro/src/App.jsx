a5ef098db8cc4e830d8f4e3fc67605b44af4e7d6
Krish A. Patel <krishpatel2067@gmail.com>
2026-07-01 12:56:34 -0400

    Step 0: Load starter; install dependencies; run app

---
+++ src/lab6-crypto-hustle-pro/src/App.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import CoinInfo from "./Components/CoinInfo"
+ 
+const API_KEY = import.meta.env.VITE_APP_API_KEY
+ 
+function App() {
+  const [list, setList] = useState(null)
+  const [filteredResults, setFilteredResults] = useState([])
+  const [searchInput, setSearchInput] = useState("")
+ 
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1",
+        {
+          headers: {
+            "x-cg-demo-api-key": API_KEY,
+          },
+        }
+      )
+      const json = await response.json()
+      setList(json)
+    }
+    fetchAllCoinData().catch(console.error)
+  }, [])
+ 
+  const searchItems = searchValue => {
+    setSearchInput(searchValue)
+    if (searchValue !== "") {
+      const filteredData = list.filter(coin =>
+        coin.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData)
+    } else {
+      setFilteredResults(list)
+    }
+  }
+ 
+  return (
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      <ul>
+        {searchInput.length > 0
+          ? filteredResults.map(coin => (
+              <CoinInfo
+                key={coin.id}
+                id={coin.id}
+                image={coin.image}
+                name={coin.name}
+                symbol={coin.symbol}
+                price={coin.current_price}
+              />
+            ))
+          : list?.map(coin => (
+              <CoinInfo
+                key={coin.id}
+                id={coin.id}
+                image={coin.image}
+                name={coin.name}
+                symbol={coin.symbol}
+                price={coin.current_price}
+              />
+            ))}
+      </ul>
+    </div>
+  )
+}
+ 
+export default App
---
+++ src/lab6-crypto-hustle-pro/src/Components/CoinInfo.jsx
@@ -0,0 +1,15 @@
+const CoinInfo = ({ id, image, name, symbol, price }) => {
+  return (
+    <li className="main-list" key={id}>
+      <img
+        className="icons"
+        src={image}
+        alt={`Small icon for ${name} crypto coin`}
+      />
+      {name} <span className="tab"></span>
+      {price != null ? ` $${price} USD` : null}
+    </li>
+  )
+}
+ 
+export default CoinInfo
