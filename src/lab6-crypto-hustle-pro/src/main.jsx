87da409d25ee166f4494004efbd1ad1417cca5d4
Krish A. Patel <krishpatel2067@gmail.com>
2026-07-01 13:06:42 -0400

    Step 1: Create routes; add common nav bar with home button

--- src/lab6-crypto-hustle-pro/src/main.jsx
+++ src/lab6-crypto-hustle-pro/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Layout from "./Routes/Layout.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
